6a4f167b857fb1de15692c727138fdd0a4f914a5
Dazaharvich <d.azaharvichp@gmail.com>
2024-08-23 18:19:29 -0400

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
-async function getData(){
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "@/lib/interface";
+import { client } from "@/lib/sanity";
+import Image from "next/image";
+
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug": slug.current,
+    titleImage
+}`;
+
+  const data = await client.fetch(query);
+
+  console.log(data);
   
+  return data;
 }
 
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
 
-export default function Home() {
+  console.log(data);
+
   return (
-   <div>
-  
-    <h1>
-      hello
-    </h1>
-   </div>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      {data.map((post, idx)=>(
+        <Card key={idx}>
+        </Card>
+      ))
+
+      }
+    </div>
   );
 }
